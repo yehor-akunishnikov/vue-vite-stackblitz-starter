adf3723ad2a8af8fe12fa717f695b40263359afb
yehor-akunishnikov <faiteforever@gmail.com>
2025-10-25 18:47:21 +0300

    Added hello

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
-<script setup></script>
-
 <template>
-  <p>Test</p>
+  <h1>Hi</h1>
+  <p>I'm <strong>your</strong> playground</p>
+  <p>Here you can write both <strong>HTML</strong> and <span class="css">CSS</span> in one file</p>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .css {
+    padding: 2px 4px;
+    border-radius: 6px;
+    font-weight: 800;
+    font-family: sans-serif;
+    color: #fdfdfd;
+    background: #2A7B9B;
+    background: linear-gradient(90deg, rgba(42, 123, 155, 1) 0%, rgba(87, 199, 133, 1) 50%, rgba(237, 221, 83, 1) 100%);
+  }
+</style>
